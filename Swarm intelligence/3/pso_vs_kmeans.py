58dbd403483219a1838d004743fff2c1d43497c5
Evgenia Martynova <evg.martynova@gmail.com>
2020-02-29 23:50:54 +0100

    Apply the algorithms and calculate statistics

--- Swarm intelligence/3/pso_vs_kmeans.py
+++ Swarm intelligence/3/pso_vs_kmeans.py
@@ -3,12 +3,14 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets
+import pandas as pd
+
 
 # Returns tuple (data, labels)
 def load_iris_dataset():
     iris = datasets.load_iris()
-    iris_data = iris['data'].tolist()
-    iris_labels = iris['target']
+    iris_data = np.array(iris['data'])
+    iris_labels = np.array(iris['target'])
 
     return iris_data, iris_labels
 
@@ -26,7 +28,7 @@ def artificial_class(data_point):
 # Returns tuple (data, labels)
 def generate_artificial_data(n_points):
     # draw 400 data point from uniform distribution in interval (-1, 1)
-    data_points = np.random.uniform(-1, 1, (n_points, 2)).tolist()
+    data_points = np.random.uniform(-1, 1, (n_points, 2))
     class_labels = [artificial_class(x) for x in data_points]
     return data_points, class_labels
 
@@ -129,7 +131,7 @@ class Particle:
 
     def update_velocity(self, global_best):
         for i in range(self.length):
-            r1, r2 = np.random.rand(2), np.random.rand(2)
+            r1, r2 = np.random.rand(self.data_dimension), np.random.rand(self.data_dimension)
             inertia = self.w * self.velocity[i]
             personal_influence = self.c1 * r1 * (self.best_position[i] - self.position[i])
             global_influence = self.c2 * r2 * (global_best[i] - self.position[i])
@@ -146,9 +148,10 @@ class Particle:
             self.best_fitness = self.fitness
             self.best_position = self.position.copy()
 
+
 class PSO_Clustering:
 
-    def __init__(self, data, n_clusters, n_particles=10, max_iter=1000, verbose=True):
+    def __init__(self, data, n_clusters, n_particles=10, max_iter=1000, verbose=False):
         self.data = data
         self.n_clusters = n_clusters
         self.n_particles = n_particles
@@ -173,7 +176,7 @@ class PSO_Clustering:
 
     def fit(self):
         for iteration in range(self.max_iter):
-            if self.verbose:
+            if self.verbose and iteration % 10 == 0:
                 print("PSO iteration {}".format(iteration))
 
             for particle in self.particles:
@@ -196,7 +199,7 @@ def k_means(data, n_clusters, max_iter=1000, verbose=False):
     centroids = data[index].copy()
 
     for iteration in range(max_iter):
-        if verbose:
+        if verbose and iteration % 10 == 0:
             print("K means iteration {}".format(iteration))
         # distribute points between clusters
         clusters = cluster_data(data, centroids)
@@ -210,56 +213,59 @@ def k_means(data, n_clusters, max_iter=1000, verbose=False):
     return clusters, centroids
 
 
-# TODO: add averaging of multiple runs, statistics
-def compare_on_iris():
-    iris_data, _ = load_iris_dataset()
-
-    k_means_clusters, k_means_centroids = k_means(iris_data, 3)
-    k_means_qe = quantization_error(k_means_centroids, k_means_clusters)
-    print("K means fitness, iris data {}".format(k_means_qe))
-
-    PSO = PSO_Clustering(iris_data, 3, max_iter=500)
-    pso_clusters, pso_centroids = PSO.fit()
-    pso_qe = quantization_error(pso_centroids, pso_clusters)
-    print("PSO fitness, iris data  {}".format(pso_qe))
-
-
-def compare_on_artificial_data():
-    artificial_data, artificial_labels = generate_artificial_data(400)
-    # Plot artificial data
-    #plot_artificial_data(artificial_data, artificial_labels)
-
-    k_means_clusters, k_means_centroids = k_means(artificial_data, 2)
-    k_means_qe = quantization_error(k_means_centroids, k_means_clusters)
-    print("K means fitness, artificial data {}".format(k_means_qe))
-    #plot_artificial_clusters(k_means_clusters)
-
-    PSO = PSO_Clustering(artificial_data, 2, max_iter=500)
-    pso_clusters, pso_centroids = PSO.fit()
-    pso_qe = quantization_error(pso_centroids, pso_clusters)
-    print("PSO fitness, artificial data  {}".format(pso_qe))
-   # plot_artificial_clusters(pso_clusters)
-
-
-def compare_on_artificial_data2():
-    artificial_data = generate_artificial_data2()
-    # Plot artificial data
-    #plot_artificial_data(artificial_data, artificial_labels)
-
-    k_means_clusters, k_means_centroids = k_means(artificial_data, 4)
-    k_means_qe = quantization_error(k_means_centroids, k_means_clusters)
-    print("K means fitness, artificial data {}".format(k_means_qe))
-    #plot_artificial_clusters(k_means_clusters)
-
-    PSO = PSO_Clustering(artificial_data, 4, max_iter=250)
-    pso_clusters, pso_centroids = PSO.fit()
-    pso_qe = quantization_error(pso_centroids, pso_clusters)
-    print("PSO fitness, artificial data  {}".format(pso_qe))
-    plot_artificial_clusters(pso_clusters)
-
-
 def main():
-    compare_on_artificial_data2()
+    random.seed(99)
+    number_of_simulations = 30
+    number_of_iterations = 1000
+
+    artificial_data, _ = generate_artificial_data(400)
+    k_means_qe_artifical = []
+    pso_qe_artifical = []
+    for simulation in range(number_of_simulations):
+        print('Artificial data simulation {}'.format(simulation+1))
+        k_means_clusters, k_means_centroids = k_means(artificial_data, 2, max_iter=number_of_iterations)
+        k_means_qe_artifical.append(quantization_error(k_means_centroids, k_means_clusters))
+
+        PSO = PSO_Clustering(artificial_data, 2, max_iter=number_of_iterations)
+        pso_clusters, pso_centroids = PSO.fit()
+        pso_qe_artifical.append(quantization_error(pso_centroids, pso_clusters))
+
+    data = {'k_means': k_means_qe_artifical, 'pso': pso_qe_artifical}
+    df = pd.DataFrame(data=data)
+    df.to_csv('artificial_stat.csv')
+
+    k_means_artifical_mean = np.mean(k_means_qe_artifical)
+    k_means_artifical_std = np.std(k_means_qe_artifical)
+    print('K-means average quantization error {}, std {}, artificial data'.format(k_means_artifical_mean, k_means_artifical_std))
+
+    pso_qe_artifical_mean = np.mean(pso_qe_artifical)
+    pso_qe_artifical_std = np.std(pso_qe_artifical)
+    print('PSO average quantization error {}, std {}, artificial data'.format(pso_qe_artifical_mean, pso_qe_artifical_std))
+
+    iris_data, _ = load_iris_dataset()
+    k_means_qe_iris = []
+    pso_qe_iris = []
+    for simulation in range(number_of_simulations):
+        print('Iris data simulation {}'.format(simulation + 1))
+        k_means_clusters, k_means_centroids = k_means(iris_data, 3, max_iter=number_of_iterations)
+        k_means_qe_iris.append(quantization_error(k_means_centroids, k_means_clusters))
+
+        PSO = PSO_Clustering(iris_data, 3, max_iter=number_of_iterations)
+        pso_clusters, pso_centroids = PSO.fit()
+        pso_qe_iris.append(quantization_error(pso_centroids, pso_clusters))
+
+    data = {'k_means': k_means_qe_iris, 'pso': pso_qe_iris}
+    df = pd.DataFrame(data=data)
+    df.to_csv('iris_stat.csv')
+
+    k_means_qe_iris_mean = np.mean(k_means_qe_iris)
+    k_means_qe_iris_std = np.std(k_means_qe_iris)
+    print('K-means average quantization error {}, std {}, iris data'.format(k_means_qe_iris_mean, k_means_qe_iris_std))
+
+    pso_qe_iris_mean = np.mean(pso_qe_iris)
+    pso_qe_iris_std = np.std(pso_qe_iris)
+    print('PSO average quantization error {}, std {}, iris data'.format(pso_qe_iris_mean, pso_qe_iris_std))
+
 
 
 if __name__ == "__main__":
