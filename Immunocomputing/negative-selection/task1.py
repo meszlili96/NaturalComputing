e0d8712f6849f647e637e661c91bee299ee7175c
Evgenia Martynova <evg.martynova@gmail.com>
2020-03-22 12:29:30 +0100

    Initial task 1 implementation

--- Immunocomputing/negative-selection/task1.py
+++ Immunocomputing/negative-selection/task1.py
@@ -1,6 +1,8 @@
 import os
 import matplotlib.pyplot as plt
 from sklearn import metrics
+import numpy as np
+import subprocess
 
 
 def create_dir(path):
@@ -10,7 +12,7 @@ def create_dir(path):
         print("Creation of the directory %s failed" % path)
 
 
-def compute_scores(n, r, training_file_path, test_file_path, is_self):
+def compute_scores_from_file(n, r, training_file_path, test_file_path, is_self):
     command = 'java -jar negsel2.jar -self {} -n {} -r {} -c -l < {}'.format(training_file_path, n, r, test_file_path)
     # process non-self (positive)
     stream = os.popen(command)
@@ -19,23 +21,56 @@ def compute_scores(n, r, training_file_path, test_file_path, is_self):
     return scores
 
 
-# returns AUC score
-def detect_non_self(r,
-                    self_file_path,
-                    non_self_file_path,
-                    output_folder,
-                    n=10,
-                    training_file_path='english.train'):
+def detect_non_self_task1(r,
+                          self_file_path,
+                          non_self_file_path,
+                          output_folder,
+                          n=10,
+                          training_file_path='english.train'):
     # process non-self (positive)
-    non_self = compute_scores(n, r, training_file_path, non_self_file_path, False)
+    non_self = compute_scores_from_file(n, r, training_file_path, non_self_file_path, False)
     positive_num = len(non_self)
 
     # process self (negative)
-    self = compute_scores(n, r, training_file_path, self_file_path, True)
+    self = compute_scores_from_file(n, r, training_file_path, self_file_path, True)
     negative_num = len(self)
 
     result = non_self + self
-    sorted_by_score = sorted(result, key=lambda tup: tup[0])
+    auc = perform_roc_analysis(result, positive_num, negative_num, output_folder, r)
+    return auc
+
+
+# test_data - a tuple (sequence, class)
+def compute_scores_from_data(n, r, training_file_path, test_data, alphabet='task 2/snd-cert/snd-cert.alpha'):
+    command = 'java -jar negsel2.jar -alphabet file://\'{}\' -self \'{}\' -n {} -r {} -c -l'.format(alphabet, training_file_path, n, r)
+
+    computed_scores = []
+    for sequence, s_class in test_data:
+        process = subprocess.Popen([command], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, close_fds=True)
+        output, error = process.communicate(sequence.encode('utf-8'))
+        scores = np.array([float(score) for score in output.rstrip().split()])
+        computed_scores.append((scores.mean(), s_class))
+        process.terminate()
+
+    return computed_scores
+
+
+def detect_non_sel_task2(r,
+                         test_data,
+                         output_folder,
+                         n=7,
+                         training_file_path='task 2/snd-cert/snd-cert.train'):
+    positive_num = len([x for x in test_data if x[1] == 1])
+    negative_num = len([x for x in test_data if x[1] == 0])
+
+    scores = compute_scores_from_data(n, r, training_file_path, test_data)
+    auc = perform_roc_analysis(scores, positive_num, negative_num, output_folder, r)
+    return auc
+
+
+# returns AUC score
+def perform_roc_analysis(data, positive_num, negative_num, output_folder, r, plot=False):
+    sorted_by_score = sorted(data, key=lambda tup: tup[0])
 
     sensitivities = []
     r_specificities = []
@@ -64,7 +99,10 @@ def detect_non_self(r,
     plt.ylabel('sensitivity')
     plt.title('Receiver operating characteristic curve')
     plt.legend(loc="lower right")
-    plt.savefig('{}/ROC curve, r={}.png'.format(output_folder, r))
+    if plot:
+        plt.show()
+    else:
+        plt.savefig('{}/ROC curve, r={}.png'.format(output_folder, r))
 
     return auc
 
@@ -75,7 +113,7 @@ def analyze_auc_by_substring(self_file, non_self_file, output_folder, substring_
     r_performance = []
     results = ''
     for r in substring_range:
-        auc = detect_non_self(r, self_file, non_self_file, output_folder)
+        auc = detect_non_self_task1(r, self_file, non_self_file, output_folder)
         print('For r = {} AUC = {} \n'.format(r, auc))
         results += 'For r = {} AUC = {} \n'.format(r, auc)
         r_performance.append((r, auc))
@@ -88,7 +126,7 @@ def analyze_auc_by_substring(self_file, non_self_file, output_folder, substring_
     results_file.close()
 
 
-def main():
+def task1():
     english_test_file = 'english.test'
 
     # Task 1.2
@@ -106,6 +144,75 @@ def main():
     print('done')
 
 
+
+def fixed_length_substrings(string, chunk_lenght):
+    substrings = []
+    for i in range(len(string)-chunk_lenght+1):
+        substrings.append(string[i:i + chunk_lenght])
+
+    return substrings
+
+
+def process_training_set(file_path):
+    samples = []
+    with open(file_path) as file:
+        for line in file:
+            samples.append(line.rstrip())
+
+    # use mim length as chunk length for now
+    min_length = np.array([len(sample) for sample in samples]).min()
+    print("Shortest string in training set {}".format(min_length))
+
+    # we want only to include unique substrings
+    training_sequences = set()
+    for sample in samples:
+        training_sequences.update(fixed_length_substrings(sample, min_length))
+
+    return training_sequences
+
+
+def write_strings_to_file(strings, file_path):
+    with open(file_path, 'w') as file:
+        for string in strings:
+            file.write(string + "\n")
+
+
+# folder - 'snd-cert' or 'snd-unm'
+def get_labeled_test_data(folder, file_index):
+    sequences = []
+    sequences_file = os.path.join('syscalls', folder, folder + '.' + str(file_index)+'.test')
+    with open(sequences_file) as file:
+        for sequence in file:
+            sequences.append(sequence.rstrip())
+
+    min_length = np.array([len(sample) for sample in sequences]).min()
+    print("Shortest string in test set {}".format(min_length))
+
+    labels = []
+    labels_file = os.path.join('syscalls', folder, folder + '.' + str(file_index) + '.labels')
+    with open(labels_file) as file:
+        for label in file:
+            labels.append(int(label.rstrip()))
+
+    return tuple(zip(sequences, labels))
+
+
+#def analyze_sequence():
+
+
+
+def main():
+    #create_dir('task 2')
+    #create_dir('task 2/snd-cert')
+
+    #training_sequences = process_training_set('syscalls/snd-cert/snd-cert.train')
+    #write_strings_to_file(training_sequences, 'task 2/snd-cert/snd-cert.train')
+
+    test_data = get_labeled_test_data('snd-cert', 1)
+    auc = detect_non_sel_task2(3, test_data, 'task 2/snd-cert/1')
+    print(auc)
+
+    print('done')
 
 
 if __name__ == "__main__":
