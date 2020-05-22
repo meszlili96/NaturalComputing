f69bd53934571970c2557ea73077a9161d59eb69
Evgenia Martynova <evg.martynova@gmail.com>
2020-05-22 10:34:48 +0200

    Add sampling from simulated distributions

--- Project/EvolutionaryGAN/simdata.py
+++ Project/EvolutionaryGAN/simdata.py
@@ -1,5 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from enum import Enum
+import random
+
+class SimulatedDistribution(Enum):
+    eight_gaussians = 1
+    twenty_five_gaussians = 2
+
 
 def eight(scale=2.):
     centers = [
@@ -23,7 +30,31 @@ def twenty_five(scale=2., size=5):
     return centers
 
 
-def gaussians(centers, grid_size=150, sigma=0.2):
+def sample(distribution: SimulatedDistribution, batch_size=1, stdev=0.2):
+    if distribution == SimulatedDistribution.eight_gaussians:
+        centers = eight()
+    elif distribution == SimulatedDistribution.twenty_five_gaussians:
+        centers = twenty_five()
+    else:
+        print("error")
+
+    dataset = []
+    for i in range(batch_size):
+        point = np.random.randn(2) * stdev
+        center = random.choice(centers)
+        point[0] += center[0]
+        point[1] += center[1]
+        dataset.append(point)
+
+    return dataset
+
+
+def data_generator(distribution: SimulatedDistribution, batch_size=1, stdev=0.1):
+    while True:
+        yield sample(distribution, batch_size, stdev)
+
+
+def gaussians_mixture(centers, grid_size=150, sigma=0.2):
     min_x = min(centers, key=lambda t: t[0])[0] - 3 * sigma
     max_x = max(centers, key=lambda t: t[0])[0] + 3 * sigma
     min_y = min(centers, key=lambda t: t[1])[1] - 3 * sigma
@@ -39,15 +70,39 @@ def gaussians(centers, grid_size=150, sigma=0.2):
 
     return g
 
-def main():
-    g1 = gaussians(eight())
-    g2 = gaussians(twenty_five())
+
+def test_gereration(distribution: SimulatedDistribution):
+    iter = 10000
+    x = []
+    y = []
+    generator = data_generator(distribution)
+    for i in range(iter+1):
+        sample = next(generator)
+        #print(sample)
+        x.append(sample[0][0])
+        y.append(sample[0][1])
+
+    plt.figure()
+    plt.scatter(x, y)
+    plt.show()
+
+
+
+def plot_sim_distribution():
+    g1 = gaussians_mixture(eight())
+    g2 = gaussians_mixture(twenty_five())
     plt.figure()
     plt.subplot(1, 2, 1)
     plt.imshow(g1)
     plt.subplot(1, 2, 2)
     plt.imshow(g2)
     plt.show()
+
+
+def main():
+    #plot_sim_distribution()
+    test_gereration(SimulatedDistribution.twenty_five_gaussians)
+
 
 if __name__ == '__main__':
     main()
