09e975f65ef882880e3ad5c2d56c1214bd7bfa1b
Evgenia Martynova <evg.martynova@gmail.com>
2020-06-05 10:35:18 +0200

    Add fitness function explanation

--- Project/EvolutionaryGAN/fitness_function.py
+++ Project/EvolutionaryGAN/fitness_function.py
@@ -3,10 +3,10 @@ import torch
 import torch.nn as nn
 
 # discriminator - discriminator network
-# loss - discriminator loss function
+# criterion - discriminator loss function
 # fake_samples - generated samples
 # real_samples - reals samples
-def egan_fitness(discriminator, loss, fake_samples, real_samples, device="cpu"):
+def egan_fitness(discriminator, criterion, fake_samples, real_samples, device="cpu"):
     # we need to compute gradient for diversity fitness score
     # is other way to do it possible?
     for param in discriminator.parameters():
@@ -18,19 +18,25 @@ def egan_fitness(discriminator, loss, fake_samples, real_samples, device="cpu"):
     # fitness quality part is just an expectation of discrimination output on fake samples
     f_q = fake_output.data.mean().cpu().numpy()
 
-    # fitness diversity computes log of discriminator gradient of
-    # expectation of logarithm of prediction on real and fake predictions
-    # TODO: understand and describe it better
-    eval_D_fake, eval_D_real = loss(fake_output, real_output)
+    # fitness diversity computes log of discriminator loss function gradient (see overleaf for more details)
+    # BCE for fake and real samples separately
+    eval_D_fake, eval_D_real = criterion(fake_output, real_output)
+    # full BCE
     eval_D = eval_D_fake + eval_D_real
-
+    
+    # gradients computation
     gradients = torch.autograd.grad(outputs=eval_D, inputs=discriminator.parameters(),
                                     grad_outputs=torch.ones(eval_D.size()).to(device),
                                     create_graph=True, retain_graph=True, only_inputs=True)
+
+    # stack gradients into array
     with torch.no_grad():
         for i, grad in enumerate(gradients):
             grad = grad.view(-1)
             allgrad = grad if i == 0 else torch.cat([allgrad, grad])
 
+    # log norm computation
     f_d = torch.log(torch.norm(allgrad)).data.cpu().numpy()
+    # for now I return it separately, because I just followed the original implementation
+    # but if we find it useless, I'd prefer to return the full score
     return f_q, f_d
