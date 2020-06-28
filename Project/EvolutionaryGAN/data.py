Polish experimental setup, add GAN evaluation metrics calculation and saving. Small fixes

Project/EvolutionaryGAN/data.py
@@ -23,7 +23,9 @@ def image_dataset(opt):
                                    transforms.ToTensor(),
                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                                ]))
+
+    return dataset
     # Create the dataloader
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size,
-                                             shuffle=True, num_workers=opt.workers)
-    return dataloader
+    #dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size,
+    #                                         shuffle=True, num_workers=opt.workers)
+    #return dataloader

Project/EvolutionaryGAN/egan.py
@@ -1,23 +1,24 @@
 import copy
-import torch.optim as optim
 from nets import *
 from data import *
 from utils import *
 from gen_losses import *
-from simdata import ToyGenerator, ToyDiscriminator, weighs_init_toy, save_sample, EightInCircle, Grid, StandardGaussian
+from simdata import ToyGenerator, ToyDiscriminator, weighs_init_toy, extract_xy
 from discr_loss import DiscriminatorLoss
 from fitness_function import egan_fitness
 
 
 class EGANOptions():
     def __init__(self, ngpu=0):
-        self.num_epochs = 50
+        self.num_epochs = 100
         self.ngpu = 0
-        self.lr = 1e-03
+        self.lr = 1e-4
         self.beta1 = 0.5
         self.beta2 = 0.999
         self.batch_size = 64
         self.workers = 1
+        # TODO: select gamma based on generator quality metrics
+        self.gamma = 0.05
         self.device = torch.device("cuda:0" if (torch.cuda.is_available() and self.ngpu > 0) else "cpu")
 
 
@@ -25,9 +26,9 @@ class ToyEGANOptions(EGANOptions):
     def __init__(self, ngpu=0):
         super().__init__(ngpu=ngpu)
         self.toy_type = 1
-        self.toy_std = 0.2
+        self.toy_std = 0.05
         self.toy_scale = 2.0
-        self.toy_len = 1000*self.batch_size
+        self.toy_len = 500*self.batch_size
 
 
 class PokeEGANOptions(EGANOptions):
@@ -44,9 +45,8 @@ class EGAN():
     __metaclass__ = ABCMeta
     def __init__(self, opt):
         self.opt = opt
-        # TODO: how do we select gamma? Fitness quality score lies in [0,1] interval,
-        #  diversity score values have higher range
-        self.gamma = 0.05
+
+        self.gamma = opt.gamma
         self.discriminator = self.create_discriminator()
         self.generator = self.create_generator()
 
@@ -65,8 +65,6 @@ class EGAN():
         # Types of selected generator losses for each training step
         self.selected_g_loss = []
 
-        self.g_sample_loglike = []
-
         # Initialize Discriminator loss functions
         self.d_loss = self.create_d_loss()
         # Initialize Generator loss functions (mutations)
@@ -81,6 +79,7 @@ class EGAN():
         self.opt_g_previous = copy.deepcopy(self.g_optimizer.state_dict())
 
         self.dataset = self.create_dataset()
+        self.data_loader = self.create_data_loader()
 
 
     # Subclasses should implement the methods below and return the corresponding objects
@@ -113,12 +112,36 @@ class EGAN():
     def create_dataset(self):
         raise NotImplementedError
 
+    """Defines and a dataloader
+    """
+    @abstractmethod
+    def create_data_loader(self):
+        raise NotImplementedError
+
     """Saves a generated sample at a specified path
        Parameters:
           path - where to save a sample
     """
     @abstractmethod
-    def save_gen_sample_func(self, path):
+    def save_gen_sample(self, path):
+        raise NotImplementedError
+
+    """Runs evaluation metrics specific to GAN
+       Parameters:
+           fake_sample - a sample for generator on fixed noise
+           real_sample - a fixed real sample
+    """
+    @abstractmethod
+    def evaluate(self, fake_sample, real_sample):
+        raise NotImplementedError
+
+    """Saves statistics specific to GAN
+       Parameters:
+            fake_sample - a sample for generator on fixed noise
+            real_sample - a fixed real sample
+    """
+    @abstractmethod
+    def save_statistics(self, fake_sample):
         raise NotImplementedError
 
     """Performs discriminator training step on a batch of real and fake samples
@@ -127,7 +150,6 @@ class EGAN():
            fake_sample - a batch of generated samples
            real_sample - a batch of real samples
     """
-
     def train_discriminator(self, fake_sample, real_sample):
         self.d_optimizer.zero_grad()
         real_prediction = self.discriminator(real_sample)
@@ -170,15 +192,17 @@ class EGAN():
         except FileExistsError:
             pass
 
-        fixed_noise = sample_noise(10000)
-        fixed_noise_ll = sample_noise(500)
+        eval_sample_size = 10000
+        fitness_sample_size = 1024
+        fixed_noise = sample_noise(eval_sample_size)
+        real_sample_fixed = self.dataset.distribution.sample(eval_sample_size)
         num_epochs = self.opt.num_epochs
         print("Starting Training Loop...")
-        steps_per_epoch = int(np.floor(len(self.dataset) / self.opt.batch_size))
+        steps_per_epoch = int(np.floor(len(self.data_loader) / self.opt.batch_size))
         for epoch in range(num_epochs):
             iter = 0
             # For each batch in the dataloader
-            for i, real_sample in enumerate(self.dataset, 0):
+            for i, real_sample in enumerate(self.data_loader, 0):
                 ############################
                 # (1) Update Discriminator network
                 ###########################
@@ -203,21 +227,23 @@ class EGAN():
                 # Mean discriminator output on the samples from candidates generators
                 fake_out_2_list = []
 
+                # Generate noise to evaluate on before and after training step, same for each offspring
+                noise = sample_noise(self.opt.batch_size)
                 # Evolutionary part. Enumerate through mutations (loss functions)
                 for _, loss in enumerate(self.g_losses_list):
                     # Copy the parameters of candidate generator and generator's optimiser
                     self.generator.load_state_dict(self.g_previous)
                     self.g_optimizer.load_state_dict(self.opt_g_previous)
-                    # Generate fake samples
-                    fake_sample = self.generator(sample_noise(self.opt.batch_size))
+
                     # Train the current generator
+                    fake_sample = self.generator(sample_noise(self.opt.batch_size))
                     g_loss, fake_out2 = self.train_generator(loss, fake_sample)
                     cand_losses_list.append(g_loss)
                     fake_out_2_list.append(fake_out2.mean().item())
 
                     # Compute fitness score on a sample after training
                     with torch.no_grad():
-                        fake_sample_trained = self.generator(sample_noise(self.opt.batch_size))
+                        fake_sample_trained = self.generator(sample_noise(fitness_sample_size))
 
                     f_q, f_d = egan_fitness(self.discriminator, self.d_loss, fake_sample_trained, real_sample.float())
                     fitness_score = f_q + self.gamma*f_d
@@ -244,55 +270,30 @@ class EGAN():
                 self.g_previous = copy.deepcopy(best_individual)
                 self.opt_g_previous = copy.deepcopy(best_individual_optim)
 
-                # save fake sample log likelihood
-                with torch.no_grad():
-                    fake_fixed_ll = self.generator(fixed_noise_ll)
-                fake_shape_ll = fake_fixed_ll.shape
-                fake_sample_fixed_ll = fake_fixed_ll.reshape((fake_shape_ll[0], fake_shape_ll[2])).numpy()
-
-                self.g_sample_loglike.append(self.target_distr().likelihood_of(fake_sample_fixed_ll))
-
                 iter += 1
-
                 # Each few iterations we plot statistics with current discriminator loss, generator loss,
                 # mean of discriminator prediction for real sample,
                 # mean of discriminator prediction for fake sample before discriminator was trained,
                 # mean of discriminator prediction for fake sample after discriminator was trained,
-                if iter % 100 == 0:
+                if iter % 250 == 0:
                     print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLast Loss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f/D(G(z)): %.4f'
                           % (epoch+1, num_epochs, iter, steps_per_epoch,
                              d_loss, self.g_losses[-1], real_out.mean().item(), fake_out.mean().item(), fake_out_2_mean))
 
-            # Check how the generator is doing by saving G's output on fixed_noise
-            # I moved it to the end of epoch, but it can be done based on iter value too
+            # After each epoch we save global statistics
+            # Sample from generator with fixed noise
             with torch.no_grad():
                 fake_fixed = self.generator(fixed_noise)
             fake_shape = fake_fixed.shape
-            self.save_gen_sample(fake_fixed.reshape((fake_shape[0], fake_shape[2])).numpy(),
-                                 "{}epoch {}.png".format(results_folder, epoch + 1))
+            fake_sample_fixed = fake_fixed.reshape((fake_shape[0], fake_shape[2])).numpy()
 
-            print("Sample log likelihood {}".format(self.target_distr().likelihood_of(fake_sample_fixed_ll)))
-            # gan.write_to_writer(fake.reshape((fake_shape[0], fake_shape[2])).numpy(),
-            #                    "epoch {}".format(epoch+1), writer, epoch)
+            # Check how the generator is doing by saving its output on fixed_noise
+            self.save_gen_sample(fake_sample_fixed, epoch, results_folder)
 
-        # Save generator's sample KDE at the end of training
-        fake = self.generator(fixed_noise).reshape((fake_shape[0], fake_shape[2])).detach().numpy()
-        # TODO: will fail for image GAN
-        save_kde(fake, self.target_distr(), results_folder)
+            # Calculate and save evaluation metrics
+            self.evaluate(fake_sample_fixed, real_sample_fixed)
 
-        # Save fake sample log likelihood in case we want to use it later
-        with open("{}fs_ll.npy".format(results_folder), 'wb') as f:
-            np.save(f, np.array(self.g_sample_loglike))
-
-        # Save fake sample log likelihood plot
-        plt.figure(figsize=(10, 5))
-        plt.title("Fake sample log likelihood")
-        plt.plot(self.g_sample_loglike)
-        plt.xlabel("iterations")
-        plt.ylabel("Log likelohood")
-        plt.savefig("{}Fake log likelihood.png".format(results_folder))
-
-        # Train statistics:
+        # Losses statistics
         plt.figure(figsize=(10, 5))
         plt.title("Generator and Discriminator Loss During Training")
         plt.plot(self.g_losses, label="G")
@@ -302,11 +303,24 @@ class EGAN():
         plt.legend()
         plt.savefig("{}train_summary.png".format(results_folder))
 
+        # At the end of training save final stats on fake sample
+        fake_fixed = self.generator(fixed_noise).reshape((fake_shape[0], fake_shape[2])).detach().numpy()
+        self.save_statistics(fake_fixed, results_folder)
+
         # Loss functions statistics:
         selected_loss_stat(self.selected_g_loss, results_folder)
 
 
 class ToyEGAN(EGAN):
+    def __init__(self, opt):
+        super().__init__(opt)
+        # Toy data evaluation metrics statistics
+        self.data_log_likelihoods = []
+        self.hq_percentage = []
+        self.stdev_x = []
+        self.stdev_y = []
+        self.js_divergence = []
+
     def create_discriminator(self):
         return ToyDiscriminator()
 
@@ -317,22 +331,101 @@ class ToyEGAN(EGAN):
         return weighs_init_toy
 
     def create_dataset(self):
-        return toy_dataset(self.opt)
+        return MixtureOfGaussiansDataset(SimulatedDistribution(self.opt.toy_type),
+                                         self.opt.toy_std,
+                                         self.opt.toy_scale,
+                                         self.opt.toy_len)
 
-    def save_gen_sample(self, sample, path):
-        save_sample(sample, path)
+    def create_data_loader(self):
+        return torch.utils.data.DataLoader(self.dataset,
+                                           batch_size=self.opt.batch_size,
+                                           num_workers=self.opt.workers)
 
-    # For KDE plot. A better way to do it might exist but I haven't found one
-    def target_distr(self):
-        distribution = SimulatedDistribution(self.opt.toy_type)
-        if distribution == SimulatedDistribution.eight_gaussians:
-            return EightInCircle(stdev=self.opt.toy_std, scale=self.opt.toy_scale)
-        elif distribution == SimulatedDistribution.twenty_five_gaussians:
-            return Grid(stdev=self.opt.toy_std, scale=self.opt.toy_scale)
-        elif distribution == SimulatedDistribution.standard_gaussian:
-            return StandardGaussian(stdev=self.opt.toy_std, scale=self.opt.toy_scale)
-        else:
-            raise ValueError
+    def save_gen_sample(self, sample, epoch, out_dir):
+        path = "{}epoch {}.png".format(out_dir, epoch + 1)
+        x, y = extract_xy(sample)
+        modes_x, modes_y = extract_xy(self.dataset.distribution.centers())
+
+        plt.figure()
+        plt.scatter(x, y, s=1.5)
+        plt.scatter(modes_x, modes_y, s=30, marker="D")
+        plt.savefig(path)
+        plt.close()
+
+    def evaluate(self, fake_sample, real_sample):
+        # Obtain fixed real data log likelihood estimate based on KDE from fake sample
+        real_data_ll = data_log_likelihood(fake_sample, real_sample, self.dataset.distribution.stdev)
+        self.data_log_likelihoods.append(real_data_ll)
+
+        # Obtain sample quality statistics
+        hq_percentage, stdev, js_diver = self.dataset.distribution.measure_sample_quality(fake_sample)
+        self.hq_percentage.append(hq_percentage)
+        self.stdev_x.append(stdev[0])
+        self.stdev_y.append(stdev[1])
+        self.js_divergence.append(js_diver)
+
+    def save_statistics(self, fake_sample, results_folder):
+        # will fail for image GAN
+        save_kde(fake_sample, self.dataset.distribution, results_folder, "test")
+
+        # Save fake sample log likelihood in case we want to use it later
+        with open("{}fs_ll.npy".format(results_folder), 'wb') as f:
+            np.save(f, np.array(self.data_log_likelihoods))
+
+        # Save fake sample log likelihood plot
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.data_log_likelihoods)
+        plt.xlabel("Epoch")
+        plt.ylabel("Data log likelohood")
+        plt.savefig("{}Real log likelihood.png".format(results_folder))
+
+        # Save fake sample log likelihood in case we want to use it later
+        with open("{}hq_rate.npy".format(results_folder), 'wb') as f:
+            np.save(f, np.array(self.hq_percentage))
+
+        # Save high quality rate plot
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.hq_percentage)
+        plt.axhline(y=1, color='tab:red')
+        plt.xlabel("Epoch")
+        plt.ylabel("High quality rate")
+        plt.savefig("{}hq_rate.png".format(results_folder))
+
+        # Save generated sample x stdev in case we want to use it later
+        with open("{}x_stdev.npy".format(results_folder), 'wb') as f:
+            np.save(f, np.array(self.stdev_x))
+
+        # Save high quality rate plot
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.stdev_x)
+        plt.axhline(y=self.dataset.distribution.stdev, color='tab:red')
+        plt.xlabel("Epoch")
+        plt.ylabel("X standard deviation")
+        plt.savefig("{}x_stdev.png".format(results_folder))
+
+        # Save generated sample y stdev in case we want to use it later
+        with open("{}y_stdev.npy".format(results_folder), 'wb') as f:
+            np.save(f, np.array(self.stdev_x))
+
+        # Save high quality rate plot
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.stdev_y)
+        plt.axhline(y=self.dataset.distribution.stdev, color='tab:red')
+        plt.xlabel("Epoch")
+        plt.ylabel("Y standard deviation")
+        plt.savefig("{}y_stdev.png".format(results_folder))
+
+        # Save generated sample JS-divergence in case we want to use it later
+        with open("{}jsd.npy".format(results_folder), 'wb') as f:
+            np.save(f, np.array(self.js_divergence))
+
+        # Save high quality rate plot
+        plt.figure(figsize=(10, 5))
+        plt.plot(self.js_divergence)
+        plt.axhline(y=0, color='tab:red')
+        plt.xlabel("Epoch")
+        plt.ylabel("JSD(nats)")
+        plt.savefig("{}jsd.png".format(results_folder))
 
 
 class PokeEGAN(EGAN):
@@ -352,7 +445,20 @@ class PokeEGAN(EGAN):
     def create_dataset(self):
         return image_dataset(self.opt)
 
-    def save_gen_sample(self, sample, path):
+    def create_data_loader(self):
+        return torch.utils.data.DataLoader(self.dataset,
+                                           batch_size=self.opt.batch_size,
+                                           shuffle=True,
+                                           num_workers=self.opt.workers)
+
+    def evaluate(self, fake_sample, real_sample):
+        pass
+
+    def save_statistics(self, fake_sample):
+        pass
+
+    def save_gen_sample(self, sample, epoch, out_dir):
+        path = "{}epoch {}.png".format(out_dir, epoch + 1)
         plt.figure()
         plt.imshow(sample)
         plt.savefig(path)
@@ -386,14 +492,14 @@ def selected_loss_stat(selected_g_losses, results_folder):
 
 def main():
     set_seed()
-    """# 8 gaussians
-    results_folder = "8 gauss egan/"
+    # 8 gaussians
+    results_folder = "8 gauss 0.2 egan/"
     # Change the default parameters if needed
     opt = ToyEGANOptions()
     # Set up your model here
     gan = ToyEGAN(opt)
     gan.train(results_folder)
-    
+    """
     # 25 gaussians
     results_folder = "25 gauss egan/"
     # Change the default parameters if needed
@@ -403,7 +509,8 @@ def main():
     gan = ToyEGAN(opt)
     gan.train(results_folder)
     """
-    
+
+    """
     #pokemon
     results_folder = "poke egan/"
     # Change the default parameters if needed
@@ -412,6 +519,8 @@ def main():
     # Set up your model here
     gan = PokeEGAN(opt)
     gan.train(results_folder)
+    """
+
     
 
 if __name__ == '__main__':

Project/EvolutionaryGAN/simdata.py
@@ -20,15 +20,15 @@ class MixtureOfGaussiansDataset(IterableDataset):
     def __init__(self, distribution: SimulatedDistribution, stdev=0.2, scale=1., length=None):
         super(MixtureOfGaussiansDataset).__init__()
         if distribution == SimulatedDistribution.eight_gaussians:
-            self.mixture_of_gaussians = EightInCircle(stdev=stdev, scale=scale, length=length)
+            self.distribution = EightInCircle(stdev=stdev, scale=scale, length=length)
         elif distribution == SimulatedDistribution.twenty_five_gaussians:
-            self.mixture_of_gaussians = Grid(stdev=stdev, scale=scale,length=length)
+            self.distribution = Grid(stdev=stdev, scale=scale,length=length)
         elif distribution == SimulatedDistribution.standard_gaussian:
-            self.mixture_of_gaussians = StandardGaussian(stdev=stdev, scale=scale, length=length)
+            self.distribution = StandardGaussian(stdev=stdev, scale=scale, length=length)
         else:
             raise ValueError
 
-        self.data_generator = self.mixture_of_gaussians.data_generator()
+        self.data_generator = self.distribution.data_generator()
         self.length = length
 
     def __iter__(self):
@@ -320,15 +320,6 @@ def weighs_init_toy(m):
     if type(m) == nn.Linear:
         nn.init.xavier_normal_(m.weight)
         m.bias.data.fill_(0.01)
-
-
-def save_sample(sample, path):
-    x, y = extract_xy(sample)
-
-    plt.figure()
-    plt.scatter(x, y, s=1.5)
-    plt.savefig(path)
-    plt.close()
 
 
 def main():

Project/EvolutionaryGAN/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 import torch
 from sklearn.neighbors import KernelDensity
 from sklearn.model_selection import GridSearchCV
@@ -19,7 +20,7 @@ def sample_noise(size):
     Returns:
         KDE
 """
-def sample_kde(sample, bandwidth=0.025):
+def sample_kde(sample, bandwidth):
     kernel = KernelDensity(bandwidth=bandwidth)
     kernel.fit(sample)
     return kernel
@@ -33,8 +34,8 @@ def sample_kde(sample, bandwidth=0.025):
     Returns:
         KDE
 """
-def data_log_likelihood(gen_sample, real_data, bandwidth=None):
-    kernel = sample_kde(gen_sample) if bandwidth is None else sample_kde(gen_sample, bandwidth)
+def data_log_likelihood(gen_sample, real_data, stdev):
+    kernel = sample_kde(gen_sample, stdev/2)
     log_likelihood = kernel.score_samples(real_data)
     return np.average(log_likelihood)
 
@@ -46,11 +47,11 @@ def data_log_likelihood(gen_sample, real_data, bandwidth=None):
         results_folder -  a path to save the results
         filename - a specific KDE name
 """
-def save_kde(sample, target_distr, results_folder, filename, bandwidth=None):
+def save_kde(sample, target_distr, results_folder, filename):
     x, y, _ = target_distr.distribution()
     xy = np.vstack([x.ravel(), y.ravel()]).T
 
-    kernel = sample_kde(sample) if bandwidth is None else sample_kde(sample, bandwidth)
+    kernel = sample_kde(sample, target_distr.stdev/2)
 
     z = np.exp(kernel.score_samples(xy)).reshape(x.shape)
     plt.figure()
@@ -72,6 +73,7 @@ def kde_cv(target_distr, bandwidths):
 
 
 def set_seed(seed=99):
+    random.seed(30)
     torch.manual_seed(seed)
     np.random.seed(seed)
 
@@ -121,11 +123,11 @@ def main():
     grid = Grid(scale=2, stdev=0.05)
     sample_real = grid.sample(5000)
     save_kde(sample_real, grid, output_dir, "25 in Grid")
-    print("Data LL, different distributions: {}".format(data_log_likelihood(sample, sample_real)))
+    print("Data LL, different distributions: {}".format(data_log_likelihood(sample, sample_real, 0.05)))
 
     eight2 = EightInCircle(scale=2, stdev=0.02)
     sample_real = eight2.sample(5000)
-    print("Data LL, similar distributions: {}".format(data_log_likelihood(sample, sample_real)))
+    print("Data LL, similar distributions: {}".format(data_log_likelihood(sample, sample_real, 0.05)))
 
     # Eight in circle bandwidth CV
     # 0.05